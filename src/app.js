fa8ab1bebe8741619aa1368d70ed2e0bdbe7703e
Lisandro H G Mosconi Fariña <lisandro.mosconi@gmail.com>
2026-02-18 20:34:36 +0000

    feat: created basic generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
+const who = ['Javi', 'Facu', 'Lisa', 'Marveen'];
+const action = ['ate', 'danced on', 'crushed', 'broke'];
+const what = ['my homework', 'my phone', 'the car'];
+const where = ['at the supermarket', 'at the hospital', 'at the graveyard', 'in Mexico', 'in a Party'];
+const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+const why = ['because he was bored', 'because a wasp sting him', 'because he was making a TikTok',]
 
+// function for selecting a random element
+const randomSelection = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// array of the arrays themselves
+const words = [who, action, what, where, when, why]
 
+// value of who for the special why
+let randomWho = randomSelection(who);
+
+// conection to the window from JS to HTML
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+// reduce for making the whole sentence
+const excuse = words.reduce((acc, arr) => {
+  return acc + " " + randomSelection(arr);
+}, "").trim();
+
+  document.getElementById("excuse").innerHTML = excuse;
+
 };
